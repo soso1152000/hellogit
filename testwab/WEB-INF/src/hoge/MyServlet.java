0040beed8e46eae8902018f31cf4fb3d575009b7
soso1152000 <soso1152000@192.168.100.100>
2014-11-04 06:39:45 +0900

    comment追加

--- testwab/WEB-INF/src/hoge/MyServlet.java
+++ testwab/WEB-INF/src/hoge/MyServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
+//test
 @WebServlet(name="mysrv",urlPatterns="/mysrv")
 public class MyServlet extends HttpServlet {
 
